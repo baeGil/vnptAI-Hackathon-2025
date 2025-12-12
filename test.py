7b572f993af469d9e680e7600cce65bfb8692639
Đỗ Hải Nam <dohainam@192.168.1.20>
2025-12-12 21:26:55 +0700

    code test file, nothing more

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-from fractions import Fraction
-def test():
-    print(Fraction(4,3))
-test()
+import sympy as sp
+
+# Định nghĩa ma trận
+ma_tran = sp.Matrix([[1, 0, 0], [2, 1, 0], [0, 2, 1]])
+
+# Tìm các giá trị riêng
+gia_tri_rieng = ma_tran.eigenvals()
+
+print("Các giá trị riêng của toán tử tuyến tính T là:", gia_tri_rieng)
